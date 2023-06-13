6ee4cb5fb9262efa956b3883d6118e2e0cd361fc
meganemartin119 <136505052+meganemartin119@users.noreply.github.com>
2023-06-13 20:15:20 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: DarkOrchid">
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: blue">
           Megangram
         </b-navbar-item>
       </template>
